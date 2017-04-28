243bf081b965c2870820e761bfa499e65d4f8eca
Andy Golding <Golding@milyli.net>
2017-04-28 10:19:22 -0500

    Initial commit for the script runner agent project

---
+++ Solutions/Milyli.ScriptRunner.Agent/Agent/ScriptRunnerAgent.cs
@@ -0,0 +1,59 @@
+﻿using Milyli.ScriptRunner.Agent.DependencyResolution;
+using Relativity.API;
+using StructureMap;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Milyli.ScriptRunner.Agent.Agent
+{
+    public abstract class ScriptRunnerAgent : kCura.Agent.AgentBase
+    {
+        private Lazy<IContainer> lazyContainer;
+
+        private IHelper helper;
+
+
+        public new IHelper Helper
+        {
+            get
+            {
+                return this.helper ?? base.Helper;
+            }
+
+            set
+            {
+                this.helper = value;
+            }
+        }
+
+        private IContainer Container
+        {
+            get
+            {
+                return this.lazyContainer.Value;
+            }
+        }
+
+        public override void Execute()
+        {
+            var agentName = this.Name.Replace(" ", string.Empty);
+            kCura.Config.Config.ApplicationName = $"Milyli.ScriptRunner::{agentName}";
+            if (this.lazyContainer == null)
+            {
+                this.lazyContainer = new Lazy<IContainer>(() => ContainerBootstrapper.Setup(this.Helper), false);
+            }
+
+            using (var childContainer = this.Container.CreateChildContainer())
+            {
+                this.Execute(childContainer);
+            }
+
+            this.RaiseMessage("Completed.", 10);
+        }
+
+        protected abstract void Execute(IContainer container);
+    }
+}
