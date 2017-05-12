2aed2243a70669c74e6506017d55685abf1a5da9
Andy Golding <Golding@milyli.net>
2017-05-12 13:13:47 -0500

    Use typeahead for the combobox for workspace selection

--- Milyli.ScriptRunner/App_Start/BundleConfig.cs
+++ Milyli.ScriptRunner/App_Start/BundleConfig.cs
@@ -13,7 +13,8 @@
 
             bundles.Add(new ScriptBundle("~/bundles/libs").Include(
                 "~/Scripts/jquery-{version}.js",
-                "~/Scripts/bootstrap.js"));
+                "~/Scripts/bootstrap.js",
+                "~/Scripts/typeahead.bundle.js"));
 
             // Use the development version of Modernizr to develop with and learn from. Then, when you're
             // ready for production, use the build tool at http://modernizr.com to pick only the tests you need.
