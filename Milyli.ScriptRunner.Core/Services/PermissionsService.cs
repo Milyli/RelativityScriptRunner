a6a9227e25b4f05bb7580eb23b9346b802cfc94b
Adam Hart <adam@milyli.com>
2017-07-10 14:41:20 -0500

    Revert to VS 2015 code style on property to fix build

--- Milyli.ScriptRunner.Core/Services/PermissionsService.cs
+++ Milyli.ScriptRunner.Core/Services/PermissionsService.cs
@@ -1,63 +1,63 @@
 ﻿namespace Milyli.ScriptRunner.Core.Services
 {
-	using System;
-	using System.Collections.Generic;
-	using System.Linq;
-	using global::Relativity.API;
-	using global::Relativity.Services;
-	using global::Relativity.Services.Permission;
-	using Milyli.ScriptRunner.Core.DependencyResolution;
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using global::Relativity.API;
+    using global::Relativity.Services;
+    using global::Relativity.Services.Permission;
+    using Milyli.ScriptRunner.Core.DependencyResolution;
 
-	public class PermissionsService : IPermissionsService
-	{
-		private bool disposedValue = false;
+    public class PermissionsService : IPermissionsService
+    {
+        private bool disposedValue = false;
         private IPermissionManager permissionManager;
 
-        public IPermissionManager PermissionManager { get => this.permissionManager; }
+        public IPermissionManager PermissionManager { get { return this.permissionManager; } }
 
         public PermissionsService(IServiceManagerFactory serviceManagerFactory)
-		{
-			this.permissionManager = serviceManagerFactory.GetServiceProxy<IPermissionManager>(ExecutionIdentity.CurrentUser);
-		}
+        {
+            this.permissionManager = serviceManagerFactory.GetServiceProxy<IPermissionManager>(ExecutionIdentity.CurrentUser);
+        }
 
-		public bool CanEdit(int workspaceId, int artifactTypeId)
-		{
-			var query = new Query();
-			var objectTypeCondition = new ObjectCondition(PermissionFieldNames.ArtifactType, ObjectConditionEnum.EqualTo, artifactTypeId);
-			var editPermissionTypeCondition = new TextCondition(PermissionFieldNames.PermissionType, TextConditionEnum.EqualTo, PermissionType.Edit.Name);
-			query.Condition = new CompositeCondition(objectTypeCondition, CompositeConditionEnum.And, editPermissionTypeCondition).ToQueryString();
-			var permissions = this.permissionManager.QueryAsync(workspaceId, query, 1).Result;
-			if (permissions.Success)
-			{
-				var permission = permissions.Results.FirstOrDefault().Artifact;
-				var result = this.permissionManager.GetPermissionSelectedAsync(workspaceId, new List<PermissionRef>() { permission }).Result;
-				return result.FirstOrDefault()?.Selected ?? false;
-			}
-			else
-			{
-				// TODO log errors
-			}
+        public bool CanEdit(int workspaceId, int artifactTypeId)
+        {
+            var query = new Query();
+            var objectTypeCondition = new ObjectCondition(PermissionFieldNames.ArtifactType, ObjectConditionEnum.EqualTo, artifactTypeId);
+            var editPermissionTypeCondition = new TextCondition(PermissionFieldNames.PermissionType, TextConditionEnum.EqualTo, PermissionType.Edit.Name);
+            query.Condition = new CompositeCondition(objectTypeCondition, CompositeConditionEnum.And, editPermissionTypeCondition).ToQueryString();
+            var permissions = this.permissionManager.QueryAsync(workspaceId, query, 1).Result;
+            if (permissions.Success)
+            {
+                var permission = permissions.Results.FirstOrDefault().Artifact;
+                var result = this.permissionManager.GetPermissionSelectedAsync(workspaceId, new List<PermissionRef>() { permission }).Result;
+                return result.FirstOrDefault()?.Selected ?? false;
+            }
+            else
+            {
+                // TODO log errors
+            }
 
-			return false;
-		}
+            return false;
+        }
 
-		public void Dispose()
-		{
-			this.Dispose(true);
-			GC.SuppressFinalize(this);
-		}
+        public void Dispose()
+        {
+            this.Dispose(true);
+            GC.SuppressFinalize(this);
+        }
 
-		protected virtual void Dispose(bool disposing)
-		{
-			if (!this.disposedValue)
-			{
-				if (disposing)
-				{
-					this.permissionManager.Dispose();
-				}
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!this.disposedValue)
+            {
+                if (disposing)
+                {
+                    this.permissionManager.Dispose();
+                }
 
-				this.disposedValue = true;
-			}
-		}
+                this.disposedValue = true;
+            }
+        }
     }
 }
