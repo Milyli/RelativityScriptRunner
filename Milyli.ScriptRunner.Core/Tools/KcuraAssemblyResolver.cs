0f3fe624b26f88189a5e19bf58847f17fa1f6f6f
Bill Harper <bill@milyli.com>
2017-09-08 16:23:07 -0500

    fix custompage for latest relativity version

---
+++ Milyli.ScriptRunner.Core/Tools/KcuraAssemblyResolver.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Linq;
+using System.Reflection;
+
+namespace Milyli.ScriptRunner.Core.Tools
+{
+	public static class KcuraAssemblyResolver
+	{
+		public static Assembly ResolveKcuraAssembly(object sender, ResolveEventArgs args)
+		{
+			var assemblyDllNames = new string[]
+			{
+				"kCura.Crypto",
+				"kCura.Data.RowDataGateway",
+				"kCura.Utility",
+				"kCura.Config",
+			};
+
+			return assemblyDllNames.Contains(args.Name) ? Assembly.Load("kCura") : null;
+		}
+	}
+}
--- Milyli.ScriptRunner.Web/Global.asax.cs
+++ Milyli.ScriptRunner.Web/Global.asax.cs
@@ -1,22 +1,25 @@
 ﻿namespace Milyli.ScriptRunner.Web
 {
-    using System.Web.Mvc;
-    using System.Web.Optimization;
-    using System.Web.Routing;
-    using Models;
+	using System;
+	using System.Web.Mvc;
+	using System.Web.Optimization;
+	using System.Web.Routing;
+	using Milyli.ScriptRunner.Core.Tools;
+	using Models;
 
-    // Note: For instructions on enabling IIS6 or IIS7 classic mode,
-    // visit http://go.microsoft.com/?LinkId=9394801
-    public class MvcApplication : System.Web.HttpApplication
-    {
-        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Performance", "CA1822:MarkMembersAsStatic", Justification = "MVC Convention")]
-        protected void Application_Start()
-        {
-            kCura.Config.Config.ApplicationName = "Milyli.ScriptRunner::CustomPage";
-            FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
-            RouteConfig.RegisterRoutes(RouteTable.Routes);
-            BundleConfig.RegisterBundles(BundleTable.Bundles);
-            ModelBinders.Binders.Add(typeof(JobScheduleModel), new JsonBinder());
-        }
-    }
+	// Note: For instructions on enabling IIS6 or IIS7 classic mode,
+	// visit http://go.microsoft.com/?LinkId=9394801
+	public class MvcApplication : System.Web.HttpApplication
+	{
+		[System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Performance", "CA1822:MarkMembersAsStatic", Justification = "MVC Convention")]
+		protected void Application_Start()
+		{
+			AppDomain.CurrentDomain.AssemblyResolve += KcuraAssemblyResolver.ResolveKcuraAssembly;
+			kCura.Config.Config.ApplicationName = "Milyli.ScriptRunner::CustomPage";
+			FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
+			RouteConfig.RegisterRoutes(RouteTable.Routes);
+			BundleConfig.RegisterBundles(BundleTable.Bundles);
+			ModelBinders.Binders.Add(typeof(JobScheduleModel), new JsonBinder());
+		}
+	}
 }
