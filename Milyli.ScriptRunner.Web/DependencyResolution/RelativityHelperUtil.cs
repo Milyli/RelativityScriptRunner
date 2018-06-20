32ada98335cdba274831e9c975629b523c1f1db3
Avi Levin <avi@milyli.com>
2018-06-20 15:24:44 -0500

    feat: enable client-side website execution

---
+++ Milyli.ScriptRunner.Web/DependencyResolution/RelativityHelperUtil.cs
@@ -0,0 +1,29 @@
+﻿namespace Milyli.ScriptRunner.Web.DependencyResolution
+{
+	using Relativity.API;
+	using System.Linq;
+
+	public static class RelativityHelperUtil
+	{
+		public static IHelper GetHelper()
+		{
+#if DEBUG
+			Milyli.ScriptRunner.ExternalIHelper.ExternalIHelper.Settings environment = null;
+			try
+			{
+				environment = Milyli.ScriptRunner.ExternalIHelper.ExternalIHelperConfigSettingsFactory.ForCurrentEnvironment();
+			}
+			catch (System.Exception ex)
+			{
+				System.Diagnostics.Trace.WriteLine(ex.ToString());
+			}
+			if (environment != null)
+			{
+				return new Milyli.ScriptRunner.ExternalIHelper.ExternalIHelper(environment);
+			}
+#endif
+
+			return Relativity.CustomPages.ConnectionHelper.Helper();
+		}
+	}
+}
