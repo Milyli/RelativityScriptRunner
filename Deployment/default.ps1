45b213ccde27112cd74c2b9b350ceea3ed1a5d02
katherine <katherine@milyli.com>
2018-09-11 15:00:50 -0500

    chore: Clean and build the project

--- Deployment/default.ps1
+++ Deployment/default.ps1
@@ -1,5 +1,35 @@
-Task Default -Depends Hello
+Properties {
+	$Deployment = Split-Path $psake.build_script_file
+	$Artifacts = "$Deployment\Artifacts"
+	$sln = "$Deployment\..\Solutions\ScriptRunner.sln"
+}
 
-Task Hello {
-	"Hello, World!"
+Task Default -Depends Rebuild
+
+Task Rebuild -Depends Clean, Build
+
+Task Build {
+	Exec { 
+		msbuild "$sln" `
+			/t:Build `
+			/p:Configuration=Release `
+			/v:quiet `
+			/p:OutDir=$Artifacts }
+}
+
+Task Clean {
+
+	if(Test-Path $Artifacts) {
+		rd $Artifacts -rec -force | out-null
+	}
+	mkdir $Artifacts | out-null
+
+	Write-Host "$sln"
+
+	Exec { 
+		msbuild "$sln" `
+			/t:Clean `
+			/p:Configuration=Release `
+			/v:quiet
+	}
 }
--- build.ps1
+++ build.ps1
@@ -1,1 +1,3 @@
-./Deployment/Tools/psake/src/psake.ps1 ./Deployment/default.ps1
+./Deployment/Tools/psake/src/psake.ps1 ./Deployment/default.ps1 `
+	-taskList Default `
+	-framework "4.7"
