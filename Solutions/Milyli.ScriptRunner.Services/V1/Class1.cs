b5c41597aa97f55d74956390e56908d383f7e19b
James Armstrong <james.armstrong@relativity.com>
2018-09-24 16:35:25 -0500

    feat: add services project and link to core and services.interfaces

---
+++ Solutions/Milyli.ScriptRunner.Services/V1/Class1.cs
@@ -0,0 +1,19 @@
+﻿namespace Milyli.ScriptRunner.Services.V1
+{
+	using System.Threading.Tasks;
+	using Interfaces.Models.Responses;
+	using Interfaces.V1;
+
+	public class ScriptManager : IScriptManager
+	{
+		public Task<ScriptResponse> GetScriptAsync(int caseId, int scriptId)
+		{
+			throw new System.NotImplementedException();
+		}
+
+		public Task<CaseScriptResponse> GetCaseScripts(int caseId)
+		{
+			throw new System.NotImplementedException();
+		}
+	}
+}
---
+++ Solutions/Milyli.ScriptRunner.Services/V1/ScriptRunManager.cs
@@ -0,0 +1,39 @@
+﻿namespace Milyli.ScriptRunner.Services.V1
+{
+	using System.Threading.Tasks;
+	using Interfaces.Models.Responses;
+	using Interfaces.V1;
+
+	public class ScriptRunManager : IScriptRunManager
+	{
+		public Task<ScriptRunResponse> GetScriptRunAsync(int scriptRunId)
+		{
+			throw new System.NotImplementedException();
+		}
+
+		public Task CreateScriptRunAsync()
+		{
+			throw new System.NotImplementedException();
+		}
+
+		public Task<RunHistoryResponse> GetRunHistoryAsync(int scriptRunId)
+		{
+			throw new System.NotImplementedException();
+		}
+
+		public Task<int> UpdateScriptRunAsync()
+		{
+			throw new System.NotImplementedException();
+		}
+
+		public Task RunAsync(int scriptRunId)
+		{
+			throw new System.NotImplementedException();
+		}
+
+		public Task RunAllAsync()
+		{
+			throw new System.NotImplementedException();
+		}
+	}
+}
