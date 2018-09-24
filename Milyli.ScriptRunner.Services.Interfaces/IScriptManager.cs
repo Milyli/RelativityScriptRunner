e6fbf229176248eb7fc079f0015dc464b59e3a38
James Armstrong <james.armstrong@relativity.com>
2018-09-24 16:02:02 -0500

    feat: add new interfaces project and initial interfaces

---
+++ Milyli.ScriptRunner.Services.Interfaces/IScriptManager.cs
@@ -0,0 +1,11 @@
+﻿namespace Milyli.ScriptRunner.Services.Interfaces
+{
+	using Relativity.Kepler.Services;
+
+	[WebService("ScriptApi")]
+	[ServiceAudience(Audience.Public)]
+	[RoutePrefix("API/Script")]
+	public interface IScriptManager
+	{
+	}
+}
---
+++ Milyli.ScriptRunner.Services.Interfaces/IScriptRunManager.cs
@@ -0,0 +1,11 @@
+﻿namespace Milyli.ScriptRunner.Services.Interfaces
+{
+	using Relativity.Kepler.Services;
+
+	[WebService("ScriptRunApi")]
+	[ServiceAudience(Audience.Public)]
+	[RoutePrefix("API/ScriptRun")]
+    public interface IScriptRunManager
+    {
+    }
+}
