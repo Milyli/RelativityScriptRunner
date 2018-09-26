22a8b265718aaf5938fa2ef6bad52dd222df5ca4
James Armstrong <james.armstrong@relativity.com>
2018-09-26 15:10:25 -0500

    doc: add xml documentation for v1 models and script manager interface

--- Milyli.ScriptRunner.Services.Interfaces/V1/Models/Input.cs
+++ Milyli.ScriptRunner.Services.Interfaces/V1/Models/Input.cs
@@ -1,13 +1,23 @@
 ﻿namespace Milyli.ScriptRunner.Services.Interfaces.V1.Models
 {
+	/// <summary>
+	/// Defines a relativity script input.
+	/// </summary>
 	public class Input
 	{
+		/// <summary>
+		/// Name of the script
+		/// </summary>
 		public string Name { get; set; }
 
-		public string InputId { get; internal set; }
-
+		/// <summary>
+		/// Data type of the input.
+		/// </summary>
 		public string InputType { get; set; }
 
+		/// <summary>
+		/// Indicates if the input is required.
+		/// </summary>
 		public bool IsRequired { get; set; }
 	}
 }
--- Milyli.ScriptRunner.Services.Interfaces/V1/Models/ScriptRun.cs
+++ Milyli.ScriptRunner.Services.Interfaces/V1/Models/ScriptRun.cs
@@ -2,20 +2,44 @@
 {
 	using System;
 
+	/// <summary>
+	/// Defines a Script Run job.
+	/// </summary>
 	public class ScriptRun
 	{
+		/// <summary>
+		/// Script Run identifier.
+		/// </summary>
 		public int Id { get; set; }
 
+		/// <summary>
+		/// Identifier of the associated script.
+		/// </summary>
 		public int RelativityScriptId { get; set; }
 
+		/// <summary>
+		/// Identifier of the script's source workspace.
+		/// </summary>
 		public int WorkspaceId { get; set; }
 
+		/// <summary>
+		/// Name of the script run job.
+		/// </summary>
 		public string Name { get; set; }
 
+		/// <summary>
+		/// Last time the script run executed, in UTC
+		/// </summary>
 		public DateTimeOffset? LastExecutionTimeUTC { get; set; }
 
+		/// <summary>
+		/// Next time the script is scheduled to execute, in UTC
+		/// </summary>
 		public DateTimeOffset? NextExecutionTimeUTC { get; set; }
 
+		/// <summary>
+		/// Status of the script run.
+		/// </summary>
 		public int JobStatus { get; set; }
 
 		/// <summary>
