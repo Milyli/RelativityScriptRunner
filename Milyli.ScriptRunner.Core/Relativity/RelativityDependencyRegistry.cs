5dd30d6a266095e1e64e2f594db34ccd405099cf
Bill Harper <bill.harper@relativity.com>
2019-05-07 14:19:33 -0500

    Update dependency registries

--- Milyli.ScriptRunner.Core/Relativity/RelativityDependencyRegistry.cs
+++ Milyli.ScriptRunner.Core/Relativity/RelativityDependencyRegistry.cs
@@ -1,20 +1,24 @@
 ﻿namespace Milyli.ScriptRunner.Core.Relativity
 {
-    using Configuration;
-    using Email;
-    using Factories;
-    using Interfaces;
-    using Repositories;
-    using Repositories.Interfaces;
-    using MilyliDependencies.Framework.Relativity;
-    using global::Relativity.API;
-    using StructureMap;
-    public class RelativityDependencyRegistry : Registry
+	using Configuration;
+	using Email;
+	using Factories;
+	using global::Relativity.API;
+	using global::Relativity.Services.Objects;
+	using Interfaces;
+	using Milyli.ScriptRunner.Core.Tools;
+	using MilyliDependencies.Framework.Relativity;
+	using Repositories;
+	using Repositories.Interfaces;
+	using StructureMap;
+
+	public class RelativityDependencyRegistry : Registry
     {
         public RelativityDependencyRegistry(IHelper helper, int workspaceId)
         {
             this.For<IHelper>().Use(helper);
             this.For<IServicesMgr>().Use(helper.GetServicesManager());
+            this.For<IObjectManager>().Use(ctx => ctx.GetInstance<IServicesMgr>().CreateProxy<IObjectManager>(ExecutionIdentity.System)).ContainerScoped();
             this.For<IRelativityContext>().Use(new RelativityContext(workspaceId));
 
             this.For<IInstanceConnectionFactory>().Use<RelativityInstanceConnectionFactory>();
@@ -24,6 +28,8 @@
             this.For<IConfigurationCryptoService>().Use<ConfigurationCryptoService>();
             this.For<IConfigurationRepository<EmailConfiguration>>().Use<ConfigurationRepository<EmailConfiguration>>();
             this.For<IEmailService>().Use<EmailService>();
-        }
+            this.For<IRelativityScriptProcessor>().Use<RelativityScriptProcessor>();
+            this.For<ISearchTableManager>().Use<SearchTableManager>();
+		}
     }
 }
