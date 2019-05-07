046d3fa5ee5e37bf8dc2a75903c2d2545dee8dc5
Bill Harper <bill.harper@relativity.com>
2019-05-07 13:13:30 -0500

    Add front-end options for direct sql execution and timeout

--- Milyli.ScriptRunner.Web/Scripts/ViewModel/JobScheduleViewModel.js
+++ Milyli.ScriptRunner.Web/Scripts/ViewModel/JobScheduleViewModel.js
@@ -30,7 +30,7 @@
                         return toTimeString(JobSchedule.LastExecutionTime());
                     });
 
-                	(function () {
+                    (function () {
                         var time = JobSchedule.ExecutionTime();
                         var minutes = Math.floor(time / 60);
                         var hours = Math.floor(minutes / 60);
@@ -51,7 +51,7 @@
                         if (meridian === "AM" && hours === 12) {
                             hours = 0;
                         }
-	                    var minutes = parseInt(inputMinutes);
+                        var minutes = parseInt(inputMinutes);
                         var timeSeconds = ((hours * 60) + minutes) * 60;
                         JobSchedule.ExecutionTime(timeSeconds);
                     }
@@ -100,15 +100,25 @@
                         return invalidInputs;
                     })
 
+										JobScheduleModel.TimeoutInvalid = ko.computed(function () {
+											return JobScheduleModel.JobSchedule.DirectSql ? JobScheduleModel.JobSchedule.MaximumRuntime < 30 : false;
+										});
+
+										JobScheduleModel.InputsInvalid = ko.computed(function () {
+											return JobScheduleModel.InvalidInputs().length > 0;
+										});
+
                     JobScheduleModel.IsInvalid = ko.computed(function () {
-                        var isInvalid = JobScheduleModel.InvalidInputs().length > 0;
-                        return isInvalid;
+											return JobScheduleModel.InputsInvalid || JobScheduleModel.TimeoutInvalid;
                     });
 
                     JobScheduleModel.InvalidText = ko.computed(function () {
-                        if (JobScheduleModel.IsInvalid()) {
+                        if (JobScheduleModel.InputsInvalid()) {
                             return "Inputs are required by invalid: " + JobScheduleModel.InvalidInputs().join(", ");
-                        }
+												}
+												else if (JobScheduleModel.TimeoutInvalid()) {
+													return "Minimum timeout is 30 seconds";
+												}
                         else {
                             return "";
                         }
@@ -123,7 +133,7 @@
                     (function () {
                         var schedule = JobScheduleModel.JobSchedule.ExecutionSchedule();
                         $.each(days, function (idx, day) {
-                        	var dayEnabled = (schedule & (1 << idx)) > 0;
+                          var dayEnabled = (schedule & (1 << idx)) > 0;
                             JobScheduleModel[day] = ko.observable(dayEnabled);
                             JobScheduleModel[day].subscribe(function (newValue) {
                                 var oldSchedule = JobScheduleModel.JobSchedule.ExecutionSchedule();
@@ -139,8 +149,8 @@
         var viewmodel = ko.viewmodel.fromModel(model, options);
         
         viewmodel.SaveJobSchedule = function () {
-        	mask();
-        	$.ajax({
+          mask();
+          $.ajax({
                 type: 'POST',
                 url: actions.Save,
                 processData: false,
